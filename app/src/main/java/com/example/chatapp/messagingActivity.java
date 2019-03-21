acde9dfa6cb1d2ec760dc856e8b7b98842f832da
Sparkadelic177 <sparkyevangelista@gmail.com>
2019-03-21 18:46:51 -0400

    messages sucessfully going to parse database

--- app/src/main/java/com/example/chatapp/messagingActivity.java
+++ app/src/main/java/com/example/chatapp/messagingActivity.java
@@ -2,11 +2,19 @@ package com.example.chatapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class messagingActivity extends AppCompatActivity {
-    long static final String USER_ID_KEY = "userId";
+    static final String USER_ID_KEY = "userId";
     static final String BODY_KEY = "body";
 
     EditText etMessage;
@@ -22,7 +30,31 @@ public class messagingActivity extends AppCompatActivity {
         btSend = findViewById(R.id.btSend);
 
 
-
+        btSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = etMessage.getText().toString();
+                ParseObject message = ParseObject.create("Message"); //creates a new class on the fly , if doesn't exist
+                message.put(USER_ID_KEY, ParseUser.getCurrentUser().getObjectId()); //makes column that points to current user
+                message.put(BODY_KEY, data); //makes a column with the data that is passed
+                message.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e == null) {
+                            Toast.makeText(messagingActivity.this, "Successfully created message on Parse",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.e("App", "Failed to save message", e);
+                        }
+                    }
+                });
+                etMessage.setText(null); //clears the input box once the data is sent
+            }
+        });
 
     }
+
+
+
 }
+
